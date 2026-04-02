e503858a47ba4db38d5ee6aa49f5b3e7622c6a4b
kjy0916 <kjykjy020916@gmail.com>
2026-04-02 11:25:12 +0900

    fix: can tx 1회 송신 성공 코드

--- Cpu0_Main.c
+++ Cpu0_Main.c
@@ -12,6 +12,7 @@
 
 IfxCpu_syncEvent cpuSyncEvent = 0;
 
+
 /*********************************************************************************************************************/
 /*------------------------------------------------------Macros-------------------------------------------------------*/
 #define LED_PORT                (&MODULE_P10)
@@ -19,14 +20,6 @@ IfxCpu_syncEvent cpuSyncEvent = 0;
 
 #define TASK_BASE_PERIOD_MS     (10U)
 #define TASK_100MS_TICKS        (10U)
-
-/* 브레이크 raw 범위 (16진수 기준) */
-#define BRAKE_RAW_MIN           (0x790U)
-#define BRAKE_RAW_MAX           (0x850U)
-
-/* 소프트웨어 PWM */
-#define PWM_PERIOD_STEPS        (100U)
-#define PWM_STEP_US             (100U)
 /*********************************************************************************************************************/
 
 /*********************************************************************************************************************/
@@ -34,16 +27,15 @@ IfxCpu_syncEvent cpuSyncEvent = 0;
 volatile uint16 g_task10msCnt   = 0U;
 volatile uint16 g_task100msCnt  = 0U;
 
-/* 디버그용 추가 변수 */
-volatile uint16 g_brakeRaw      = 0U;
-volatile uint16 g_ledDuty       = 0U;
+/* 디버그용 변수 */
+volatile boolean g_ledOn        = FALSE;
+volatile boolean g_buttonPulse  = FALSE;
 /*********************************************************************************************************************/
 
 /*********************************************************************************************************************/
 /*------------------------------------------------Function Prototypes------------------------------------------------*/
 static void InitLed(void);
-static uint16 MapBrakeToDuty(uint16 raw);
-static void RunLedPwm(uint16 duty);
+static void ToggleLed(void);
 /*********************************************************************************************************************/
 
 /*********************************************************************************************************************/
@@ -57,6 +49,13 @@ void core0_main(void)
     IfxScuWdt_disableCpuWatchdog(IfxScuWdt_getCpuWatchdogPassword());
     IfxScuWdt_disableSafetyWatchdog(IfxScuWdt_getSafetyWatchdogPassword());
 
+    /* CAN transceiver STB(P20.6) 강제 normal mode */
+    IfxPort_setPinModeOutput(&MODULE_P20,
+                             6,
+                             IfxPort_OutputMode_pushPull,
+                             IfxPort_OutputIdx_general);
+    IfxPort_setPinLow(&MODULE_P20, 6);
+
     IfxCpu_emitEvent(&cpuSyncEvent);
     IfxCpu_waitEvent(&cpuSyncEvent, 1);
 
@@ -69,17 +68,20 @@ void core0_main(void)
         g_task10msCnt++;
 
         inputData = If_InputEcu_GetData();
-        g_brakeRaw = inputData->brake_raw;
-        g_ledDuty = MapBrakeToDuty(g_brakeRaw);
+        g_buttonPulse = (inputData->user_ack_button == true) ? TRUE : FALSE;
 
-        /* 10ms 주기 안에서 LED 밝기 제어 */
-        RunLedPwm(g_ledDuty);
+        if (inputData->user_ack_button == true)
+        {
+            ToggleLed();
+        }
 
         if ((g_task10msCnt % TASK_100MS_TICKS) == 0U)
         {
             App_InputEcu_Task_100ms();
             g_task100msCnt++;
         }
+
+        waitTime(IfxStm_getTicksFromMilliseconds(BSP_DEFAULT_TIMER, TASK_BASE_PERIOD_MS));
     }
 }
 
@@ -90,53 +92,23 @@ static void InitLed(void)
                              LED_PIN,
                              IfxPort_OutputMode_pushPull,
                              IfxPort_OutputIdx_general);
+
     IfxPort_setPinLow(LED_PORT, LED_PIN);
+    g_ledOn = FALSE;
 }
 
 /*********************************************************************************************************************/
-static uint16 MapBrakeToDuty(uint16 raw)
+static void ToggleLed(void)
 {
-    uint32 value;
-
-    if (raw <= BRAKE_RAW_MIN)
+    if (g_ledOn == FALSE)
     {
-        return 0U;
-    }
-    else if (raw >= BRAKE_RAW_MAX)
-    {
-        return PWM_PERIOD_STEPS;
+        IfxPort_setPinHigh(LED_PORT, LED_PIN);
+        g_ledOn = TRUE;
     }
     else
     {
-        value = ((uint32)(raw - BRAKE_RAW_MIN) * PWM_PERIOD_STEPS) /
-                (uint32)(BRAKE_RAW_MAX - BRAKE_RAW_MIN);
-
-        return (uint16)value;
-    }
-}
-
-/*********************************************************************************************************************/
-static void RunLedPwm(uint16 duty)
-{
-    uint16 i;
-
-    if (duty > PWM_PERIOD_STEPS)
-    {
-        duty = PWM_PERIOD_STEPS;
-    }
-
-    for (i = 0U; i < PWM_PERIOD_STEPS; i++)
-    {
-        if (i < duty)
-        {
-            IfxPort_setPinHigh(LED_PORT, LED_PIN);
-        }
-        else
-        {
-            IfxPort_setPinLow(LED_PORT, LED_PIN);
-        }
-
-        waitTime(IfxStm_getTicksFromMicroseconds(BSP_DEFAULT_TIMER, PWM_STEP_US));
+        IfxPort_setPinLow(LED_PORT, LED_PIN);
+        g_ledOn = FALSE;
     }
 }
 /*********************************************************************************************************************/
